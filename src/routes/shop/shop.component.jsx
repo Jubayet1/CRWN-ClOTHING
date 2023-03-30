8912bb02ff81d368d89f6516af83d64ef354b8d2
jubayet <jubayet0106@gmail.com>
2023-03-30 16:51:01 +0600

    storing products on firestore

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,16 +1,16 @@
-import { useContext } from "react";
+import { useContext } from 'react';
 
-import ProductCard from "../../components/product-card/product-card.component";
+import ProductCard from '../../components/product-card/product-card.component';
 
-import { ProductsContext } from "../../contexts/products.context";
+import { ProductsContext } from '../../contexts/products.context';
 
-import "./shop.styles.scss";
+import './shop.styles.scss';
 
 const Shop = () => {
   const { products } = useContext(ProductsContext);
 
   return (
-    <div className="products-container">
+    <div className='products-container'>
       {products.map((product) => (
         <ProductCard key={product.id} product={product} />
       ))}
